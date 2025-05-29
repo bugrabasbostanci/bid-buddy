084b474d88f9be432a4f36c4e8ddba3a06b2a027
bugrabasbostanci <bb0143sbw+github@gmail.com>
2025-05-29 20:56:40 +0300

    add in next-auth and items table

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,41 @@
 import { Button } from "@/components/ui/button";
 import { database } from "./db/database";
-import { bids as bidsSchema } from "./db/schema";
+import { items } from "./db/schema";
 import { Input } from "@/components/ui/input";
 import { revalidatePath } from "next/cache";
+import SignIn from "@/components/sign-in";
+import { SignOut } from "@/components/sign-out";
+import { auth } from "@/auth";
 
 export default async function HomePage() {
+  const session = await auth()
 
-  const bids = await database.query.bids.findMany()
+  const allItems = await database.select().from(items);
+  // const allItems = await database.query.items.findMany();
 
-  return <main className="container mx-auto py-12">
-    <form action={async(formData: FormData) => {
-      "use server"
-      // const bid = formData.get("bid") as string;
-      await database.insert(bidsSchema).values({})
-      revalidatePath("/")
-    }}>
-      <Input name="bid"  placeholder="Bid" />
-      <Button type="submit" >Place Bid</Button>
-    </form>
+  return (
+    <main className="container mx-auto py-12">
+      {session ? <SignOut /> : <SignIn />}
 
-    {bids.map(bid => (
-      <div key={bid.id}>{bid.id}</div>
-    ))}
-  </main>
+      {session?.user?.name}
+      
+      
+      <form action={async (formData: FormData) => {
+        "use server"
+        // const bid = formData.get("bid") as string;
+        await database.insert(items).values({
+          name: formData.get("name") as string,
+          userId: session?.user?.id!,
+        })
+        revalidatePath("/")
+      }}>
+        <Input name="name" placeholder="Name your item" />
+        <Button type="submit" >Post Item</Button>
+      </form>
+
+      {allItems.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+    </main>
+  )
 }
